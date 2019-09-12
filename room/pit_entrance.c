e8f33cf91d9f67bf5496f163c4ac62eb7836014f
barryeee <eichelbergerb@nicc.edu>
2019-09-12 13:45:16 -0500

    develop pit_stairs.
    Signed-off-by: barryeee <eichelbergerb@nicc.edu>

--- room/pit_entrance.c
+++ room/pit_entrance.c
@@ -3,29 +3,29 @@
 
 inherit LIB_ROOM;
 
-
+//int SlipnSlide ();
 
 void create() {
     room::create();
     SetAmbientLight(30);
     SetShort("Entrance to the Pit");
     SetLong("a sparsely furnished  room where the guard keeps track of high priority prisoners. ");
-    SetInventory(([
-     
+    SetInventory(([     
         "/domains/AK/npc/orc1" : 1,
 		"/domains/AK/npc/gargoyle" : 1,
 		"/domains/AK/obj/bench" : 1,
       ]));
-    SetExits( ([
+    
+	SetExits( ([
         "west" : "/domains/AK/room/hall4",
-       ]) );
-
+		"east" : "/domains/Ak/room/pit_stairs",
+	]) );
+//	AddExit("east" : "/domains/AK/room/pit_stairs");
+	
     SetClimate("indoors");
    
 }
 void init(){
     ::init();
-	
+
 }
-
-
--- room/pit_stairs.c
+++ room/pit_stairs.c
@@ -1,20 +1,42 @@
 #include <lib.h>
-#include <climb.h> // defines CLIMB_DOWN
+#include ROOMS_H
 
-inherit LIB_ITEM;
-inherit LIB_CLIMB;
+inherit LIB_ROOM;
 
-static void create() {
-    ::create();
-    SetKeyName("stairs");
-    SetId("standard");
-    SetAdjectives(({"steep","stone","narrow","slippery"}));
-    SetShort("a ladder");
-    SetLong("This is a narrow, stone stairway,which looks rather slippery.  Caution is advised!");
-    SetMass(100);
-    SetBaseCost("silver",10);
+int SlipnSlide();
+
+void create() {
+    room::create();
+    SetAmbientLight(20);
+    SetShort("stairs");
+    SetLong("A steep stone stairway, covered in slime.");
+    SetItems(([
+        
+      ]));
+
+    SetExits(([
+        "west" : "/domains/AK/room/Pit_entrance",
+      	
+		]));
+	
+	AddExit("east", "/domains/AK/room/pit_entrance", ( : SlipnSlide  :));
+		
+    SetInventory(([
+      ]));
+
+    SetClimate("indoors");
 }
-
 void init(){
     ::init();
 }
+//A test to detect the presence of a rock from the prison cell on the player.
+//If the player has a rock, they may enter the room to the east.
+//if not, they are tranported to the begining of the maze, concealing the exit to the pit entrance.
+int SlipnSlide() {
+	if ((random(10)+1) < 15) {
+		write("You have slipped and fallen to the bottom of the stairs.");
+		 this_player()->eventMoveLiving("/domains/AK/room/pit_entrance");
+        return 0;
+	}
+	return 1;
+ } 
