d17c460ebffd965c2488623765d3ce5210e4553a
barryeee <eichelbergerb@nicc.edu>
2019-10-17 12:39:58 -0500

    Added jail cells.
    Signed-off-by: barryeee <eichelbergerb@nicc.edu>

---
+++ room/jail2.c
@@ -0,0 +1,26 @@
+#include <lib.h>
+#include ROOMS_H
+
+inherit LIB_ROOM;
+
+
+
+void create() {
+    room::create();
+    SetAmbientLight(20);
+    SetShort("Jail Cell");
+    SetLong("This is the place where ordinary criminals are held. Since you are here, you must be an ordinary criminal.
+	There is nothing to write home about concerning this cell. Stone walls,damp, and dreary.");
+         
+	SetExits( ([
+    	"north" : "/domains/AK/room/jail",
+		]) );
+
+
+    SetClimate("indoors");
+   
+}
+void init(){
+    ::init();
+
+}
